5249c64ef3c9d566a7152216838e9515aa69cc38
fly <qifei1992@sina.cn>
2022-01-20 00:17:07 +0800

    通过APT模块绑定布局成功

--- app/src/main/java/com/gpf/learnapt/MainActivity.java
+++ app/src/main/java/com/gpf/learnapt/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.gpf.annotation.GPFBindLayout;
+import com.gpf.sdk.GPFApi;
 
 @GPFBindLayout(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        GPFApi.bindLayout(this);
+//        MainActivity_ViewBindLayout mainActivity_viewBindLayout = new MainActivity_ViewBindLayout();
+//        mainActivity_viewBindLayout.setContentView(this);
+//        setContentView(R.layout.activity_main);
     }
 }
--- apt-processor/src/main/java/com/gpf/processor/GPFLayoutProcessor.java
+++ apt-processor/src/main/java/com/gpf/processor/GPFLayoutProcessor.java
@@ -2,6 +2,11 @@ package com.gpf.processor;
 
 import com.google.auto.service.AutoService;
 import com.gpf.annotation.GPFBindLayout;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.TypeSpec;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -16,6 +21,7 @@ import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.tools.Diagnostic;
@@ -59,40 +65,72 @@ public class GPFLayoutProcessor extends AbstractProcessor {
         // 找到含有 GPFBindLayout 注解的所有元素
         Set<? extends Element> elements =
                 roundEnvironment.getElementsAnnotatedWith(GPFBindLayout.class);
-        info("elements size = " + elements.size());
         for (Element e :
                 elements) {
-            info("element kind = " + e.getKind().name() + "/" +
-                    e.getSimpleName());
-
-            info("element type = " + e.asType().toString());
 
             GPFBindLayout bindLayout = e.getAnnotation(GPFBindLayout.class);
 
             String newClassName = e.getSimpleName() + "_ViewBindLayout";
 
-            StringBuilder builder = new StringBuilder()
-                    .append("package com.gpf.processor.auto;\n\n")
-                    .append("public class ")
-                    .append(newClassName)
-                    .append(" {\n\n") // open class
-                    .append("\tpublic void setContentView() {\n"); // open method
+            // 使用 Javapoet
 
-            // TODO 具体的方法实现
+            ClassName activityClassName = ClassName.get("android.app", "Activity");
 
-            builder.append("\t}\n") // close method
-                    .append("} \n"); // end class
+            // 定义参数 Activity activity
+            ParameterSpec parameterSpec = ParameterSpec
+                    .builder(activityClassName, "activity").build();
 
-            try { // write the file
-                JavaFileObject source = filer.createSourceFile("com.gpf.processor.auto." + newClassName);
-                Writer writer = source.openWriter();
-                writer.write(builder.toString());
-                writer.flush();
-                writer.close();
-            } catch (IOException ioe) {
-                // Note: calling e.printStackTrace() will print IO errors
-                // that occur from the file already existing after its first run, this is normal
+            // 方法
+            MethodSpec setContentView = MethodSpec.methodBuilder("setContentView")
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(void.class)
+                    .addParameter(parameterSpec)
+//                    .addStatement("activity.setContentView(" + bindLayout.value() + ")")
+                    .addStatement("activity.setContentView($L)", bindLayout.value())
+                    .build();
+
+            // 生成Java源文件类
+            TypeSpec viewBindLayout = TypeSpec.classBuilder(newClassName)
+                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                    .addMethod(setContentView)
+                    .build();
+
+            // 输出Java源文件
+            JavaFile javaFile = JavaFile
+                    .builder("com.gpf.learnapt", viewBindLayout)
+                    .build();
+
+            try {
+                javaFile.writeTo(filer);
+                info(javaFile.toString());
+                info("文件生成成功");
+            } catch (IOException ioException) {
+                info("文件生成失败");
             }
+
+//            StringBuilder builder = new StringBuilder()
+//                    .append("package com.gpf.learnapt;\n\n")
+//                    .append("public class ")
+//                    .append(newClassName)
+//                    .append(" {\n\n") // open class
+//                    .append("\tpublic void setContentView() {\n"); // open method
+//
+//            // TODO 具体的方法实现 方法内部调用 activity.setContentView(bindLayout.value())
+//            // setContentView 需要一个参数 activity
+//
+//            builder.append("\t}\n") // close method
+//                    .append("} \n"); // end class
+//
+//            try { // write the file
+//                JavaFileObject source = filer.createSourceFile("com.gpf.processor.auto." + newClassName);
+//                Writer writer = source.openWriter();
+//                writer.write(builder.toString());
+//                writer.flush();
+//                writer.close();
+//            } catch (IOException ioe) {
+//                // Note: calling e.printStackTrace() will print IO errors
+//                // that occur from the file already existing after its first run, this is normal
+//            }
 
         }
 
